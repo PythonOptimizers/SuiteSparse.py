b318146af3c4037981ea94f3606411c9d3014167
counterclocker <nikolaj.van.omme@gmail.com>
2016-01-13 00:10:01 -0500

    added basic solve and solve_default DOES NOT WORK

--- examples/spqr.py
+++ examples/spqr.py
@@ -6,20 +6,39 @@ import numpy as np
 import time
 
 size = 5
+nrow = 5
+ncol = 5
+
 itype = INT32_T #INT64_T
 dtype = FLOAT64_T #COMPLEX128_T
 
 np_dtype = np.float64 #complex128
 
 start_time = time.clock()
-A = LinearFillLLSparseMatrix(ncol=size, nrow=size, dtype=dtype, itype=itype, store_symmetric=True)
+A = ArrowheadLLSparseMatrix(ncol=ncol, nrow=nrow, dtype=dtype, itype=itype)
+
+print A
+
+AA = A.to_ndarray()
+
+print "Determinant = %f" % np.linalg.det(AA)
+
 
 print "construction time for matrix A : %f" % (time.clock() - start_time)
 
 start_time = time.clock()
+s = SPQRSolver(A, verbose=True)
+
 print "construction time for solver A : %f" % (time.clock() - start_time)
 
-s = SPQRSolver(A, verbose=True)
+
+
+b = np.ones(nrow, np_dtype)
+
+x = s.solve_default(b)
+
+print x
+print A * x
 
 print spqr_version()
 print spqr_detailed_version()
